e55974d63d683ffeb5637dd7b56e063fb55cf08f
Ludovic <ludovic.lachal@ynov.com>
2023-10-11 18:45:27 +0200

    Apropos: scroll on click fait + responsive member img finit/Ajout police sur Accueil et Apropos

---
+++ Apropos/apropos.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('disapearBtn').addEventListener('click', function() {
+        this.style.display = 'none';
+    });
+});
